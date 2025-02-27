82d3c7be87de6bd418f4ec2bf687f13bac1a5d08
Shreyes Prabhu Desai <shreyes.desai@zapcg.com>
2025-02-27 13:34:11 +0530

    testing git re config

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -57,6 +57,18 @@ export default function AboutUs() {
                         </div>
                     </div>
                 </div>
+                <div className="accordion-item" style={style}>
+                    <h2 className="accordion-header">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree" style={style}>
+                            <b>Accordion Item #3</b>
+                        </button>
+                    </h2>
+                    <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample" style={style}>
+                        <div className="accordion-body" style={style}>
+                            <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classNamees that we use to style each element. These classNamees control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                        </div>
+                    </div>
+                </div>
             </div >
             <div className="container my-3">
                 <button type='button' className="btn btn-primary my-3" onClick={changeBG}>{buttonText}</button>
